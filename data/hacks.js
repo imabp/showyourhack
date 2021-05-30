0b89cadbb89bed649a119c23d89d123b427589c4
Siddharth Shah <siddharthsshah1207@gmail.com>
2021-05-30 23:59:51 +0530

    hifaazat_sid-1207

--- data/hacks.js
+++ data/hacks.js
@@ -6,7 +6,12 @@ let hacks = [
     "Github": "Project GitHub/GitLab Link.",
     "Website" : "If any Website, is available, you can share the link."
   },
-  
+  {
+    "Hackathon_Name": "HackOn_2.0",
+    "Project_Name": "hifaazat",
+    "Description":"As the name suggests 'hifaazat', our application is focused on safety. It is an android application built with flutter and firebase. This application notifies the user's saved contacts or the police in case of any danger.The aim was to create an app that is easy to use and has all the useful features in one place, unlike other safety applications.", 
+    "Github":"https://github.com/sid-1207/hifaazat",
+  },  
 ]
 
 
